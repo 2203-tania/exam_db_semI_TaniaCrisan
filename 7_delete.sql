6ce213e8d73aef2ff3bb8bcd50ec4ab880ff0df4
2203-tania <tania.crisan02@e-uvt.ro>
2025-02-07 11:20:46 +0200

    Update 7_delete.sql

--- 7_delete.sql
+++ 7_delete.sql
@@ -14,7 +14,6 @@ DELETE FROM tools WHERE price > 5000;
 DELETE FROM tools WHERE tool_type = 'Plantare';
 
 -- delete employees
-select *from employees
 DELETE FROM employees WHERE salary < 35000;
 DELETE FROM employees WHERE employee_role = 'Administrator';
 DELETE FROM employees WHERE id_farm = 7;
@@ -26,14 +25,19 @@ DELETE FROM tasks WHERE description LIKE 'T%';
 
 --delete crops_pest_and_disease
 DELETE FROM crops_pest_and_disease WHERE id IN (1,7);
+DELETE FROM crops_pest_and_disease WHERE id_pest_and_disease=6;
 
 -- delete pest_and_disease
 DELETE FROM pest_and_disease WHERE pest_disease_type = 'Insectă' and id in (2,6);
 DELETE FROM pest_and_disease WHERE pest_disease_type like 'B%' AND id=2;
 DELETE FROM pest_and_disease WHERE nomenclature like '%vie';
 
+-- delete sales 
+DELETE FROM sales WHERE id=12;
+DELETE FROM sales WHERE  items LIKE 'C%';
+DELETE FROM sales WHERE quantity IN (SELECT price from animals WHERE sale_date >'2024-09-05');
+
 -- delete crops
-select * from crops
 DELETE FROM crops WHERE id = (SELECT MAX(id) FROM crops);
 DELETE FROM crops WHERE crop_type='Mar verde';
 DELETE FROM crops WHERE growth_stage='Coacere' AND planting_date='2024-07-01';
@@ -43,8 +47,4 @@ DELETE FROM animals WHERE id > 10;
 DELETE FROM animals WHERE health_status LIKE 'B%';
 DELETE FROM animals WHERE breed IN (SELECT health_status FROM animals WHERE animal_type='Oaie');
 
--- delete sales 
-DELETE FROM sales WHERE id <2;
-DELETE FROM sales WHERE  items LIKE 'C%';
-DELETE FROM sales WHERE quantity IN (SELECT price from animals WHERE sale_date >'2024-09-05');
 
